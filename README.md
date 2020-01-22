c2cf12dfce51e1e29505ac517a158d351b9067d4
KennyChen <kennychen851228@gmail.com>
2020-01-22 23:20:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## one to one chatting room
+
+1. for one client call http://localhost:8080/ws?senderId=Kenny&receiverId=Nicole
+2. for another client call http://localhost:8080/ws?senderId=Nicole&receiverId=Kenny
+3. then can talk each other
+
+for test, you can use chrome extension: smart-websocket-client 
---
+++ hub.go
@@ -0,0 +1,55 @@
+package main
+
+type Message struct {
+	Sender string
+	Receiver string
+	Content []byte
+}
+
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	broadcast chan *Message
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan *Message),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				if client.id == message.Receiver {
+					select {
+					case client.send <- message.Content:
+					default:
+						close(client.send)
+						delete(h.clients, client)
+					}
+				}
+			}
+		}
+	}
+}
